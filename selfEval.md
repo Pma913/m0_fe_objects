14193c7abeaa1b1f5e68dbc64e260f05a51800a2
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-01-12 21:06:09 -0500

    Include completion of all exercises

--- selfEval.md
+++ selfEval.md
@@ -1,5 +1,13 @@
 # Self Evaluation
 
 - What did you struggle with in this section?
+
+I struggled most with calling out a single key value and listing the Object as a whole. 
+
 - How is note-taking going? What are you finding helpful? When do you find yourself taking the most helpful notes?
+
+I feel note taking has taken a downword spiral. They have become less coherent and I rely on the lesson plans and the replits more than anything. I am finding that I wish during lessons I could make a diagram of the subject (i.e. functions) to refer back to when making new ones.
+
 - What are you noticing that you lean into - Googling, or trying to solve a problem in code on your own first?
+
+Solve problem on my own for sure! I find googling to be difficult most of the times. I feel that a lot of what I come across is formatted differently and tends to confuse me.
--- syntaxCleanUp.js
+++ syntaxCleanUp.js
@@ -1,22 +1,31 @@
-var foods = {apples: 23, 
+// // I adjusted the keys down one line so they could all be within the object and moved brackets to correct places.
+var foods = {
+  apples: 23, 
   grapes: 507, 
-  eggs: 48}
+  eggs: 48,
+}
 
-var person = { height: "6 ft", weight: "160 lbs" }
+// // all data was on one line. moved keys and their values to their own lines and adjusted brackets. Comma after value for "weight" key.
+var person = { 
+  height: "6 ft", 
+  weight: "160 lbs", 
+}
 
-var post = { imageSrc: "./images/beach.png",
+// // I moved key down to its own line and adjusted brackets accordingly. straightened out lower keys and brought 
+// // them up to respectable location within Oject. Moved "timestamp" key back in line with others.
+var post = { 
+  imageSrc: "./images/beach.png",
+  caption: "At the beach with my besties", 
+  timestamp: "4:37 PM August 13, 2019",
+  number_likes: 0,
+  comments: [] 
+}
 
-
-   caption: "At the beach with my besties", timestamp: "4:37 PM August 13, 2019",
-        number_likes: 0,
-comments: [] }
-
-
-var post2 =
-{
-      imageSrc: "./images/holiday-party.png",
-      caption: "What a great holiday party omg",
-      timestamp: "11:37 PM December 31, 2019",
-      numberLikes: 13,
-      comments: []
+// // moved all keys back to have a single tab indented
+var post2 = {
+  imageSrc: "./images/holiday-party.png",
+  caption: "What a great holiday party omg",
+  timestamp: "11:37 PM December 31, 2019",
+  numberLikes: 13,
+  comments: []
 }
